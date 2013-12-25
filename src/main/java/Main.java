2ea15dc43a953fe99a02f232766868e7e266f959
Ryan Yan <rawbenny@gmail.com>
2013-12-25 17:02:44 +0800

    fix index

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,34 @@
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.*;
 import org.eclipse.jetty.servlet.*;
+import org.eclipse.jetty.server.handler.*;
 
 public class Main extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getWriter().print("Hello from Java!\n");
-    }
-    public static void main(String[] args) throws Exception{
-        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new Main()),"/Hello");
-        server.start();
-        server.join();
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().print("Hello from Java!\n");
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(
+				ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+
+		ResourceHandler resource_handler = new ResourceHandler();
+		resource_handler.setDirectoriesListed(true);
+		resource_handler.setWelcomeFiles(new String[] { "index.html" });
+		resource_handler.setResourceBase(".");
+
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { resource_handler, context });
+		server.setHandler(handlers);
+
+		context.addServlet(new ServletHolder(new Main()), "/Hello");
+		server.start();
+		server.join();
+	}
 }
